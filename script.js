bb0d421685927df1f7557f93fc85f5cbc003ee24
Thiago José Alves de Souza <thijalves@gmail.com>
2022-11-19 12:41:36 -0300

    previsoes de ph e umidade adicionadas

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiUrl = "https://labradorserver.herokuapp.com";
 
 // setTimeout("location.reload(true);", 10000);
 
-function makeChart(canvasID, xValues, yValues, min, max){
+function makeChart(canvasID, xValues, yValues, yValues2, min, max){
   new Chart(canvasID, {
     type: "line",
     data: {
@@ -14,6 +14,12 @@ function makeChart(canvasID, xValues, yValues, min, max){
           backgroundColor: "rgba(0,0,255,1.0)",
           borderColor: "rgba(0,0,255,0.1)",
           data: yValues,
+        },{
+          fill: false,
+          lineTension: 0,
+          backgroundColor: "rgba(0,255,0,1.0)",
+          borderColor: "rgba(0,255,0,0.5)",
+          data: yValues2,
         },
       ],
     },
@@ -27,11 +33,15 @@ function makeChart(canvasID, xValues, yValues, min, max){
 }
 
 async function update() {
+
+  const foreCastResonse = await fetch(apiUrl + "/forecasts/11");
+  var foreData = await foreCastResonse.json();
+
   //get data from backend
   const telemetrResponse = await fetch(apiUrl + "/latestTelemetry");
   const tempResponse = await fetch(apiUrl + "/chartData/temperature/11");
   const pHResponse = await fetch(apiUrl + "/chartData/pH/11");
-  const humiResponse = await fetch(apiUrl + "/chartData/pH/11");
+  const humiResponse = await fetch(apiUrl + "/chartData/humidity/11");
 
   //convert data
   var telemetryData = await telemetrResponse.json();
@@ -52,20 +62,23 @@ async function update() {
 
   var xValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
   var yValues = [];
+  var yValuesPred = [,,,,,,,foreData[0].pHForecast,foreData[1].pHForecast,foreData[2].pHForecast, foreData[3].pHForecast];
   
   //pH graph
-  for(let i = 0; i < 11; i++)
+  for(let i = 0; i < 8; i++)
     yValues[i] = pHData[i].pH;
-  makeChart(pH,xValues, yValues, 0, 14);
+  makeChart(pH,xValues, yValues, yValuesPred, 0, 14);
 
+  var yValuesPred = [,,,,,,,foreData[0].airHumForecast,foreData[1].airHumForecast,foreData[2].airHumForecast, foreData[3].airHumForecast];
   //humidity graph
-  for(let i = 0; i < 11; i++)
-    yValues[i] = humiData[i].pH;
-  makeChart(humidity,xValues, yValues, 0, 100);
+  for(let i = 0; i < 8; i++)
+    yValues[i] = humiData[i].humidity;
+  makeChart(humidity,xValues, yValues, yValuesPred, 0, 100);
 
-  //temperature graph
-  for(let i = 0; i < 11; i++)
-    yValues[i] = tempData[i].temperature;
-  makeChart(temperature,xValues, yValues, 0, 60);
+
+  // //temperature graph
+  // for(let i = 0; i < 11; i++)
+  //   yValues[i] = tempData[i].temperature;
+  // makeChart(temperature,xValues, yValues, 0, 60);
 
 }
